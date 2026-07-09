3efaf268676c559cbe066decbdda69730eedeeac
Oleg Kasyanov <oleg@batumi.org>
2026-07-09 22:28:05 +0400

    Updated Georgian

--- languages/georgian/catalog/account/transaction.php
+++ languages/georgian/catalog/account/transaction.php
@@ -6,7 +6,7 @@ $_['heading_title']      = 'თქვენი ტრანზაქციებ
 $_['text_account']       = 'ანგარიში';
 $_['text_transaction']   = 'თქვენი ტრანზაქციები';
 $_['text_total']         = 'თქვენი მიმდინარე ბალანსი არის:';
-/* LM ADDED */ $_['text_no_results']    = 'You do not have any transactions!';
+$_['text_no_results']    = 'You do not have any transactions!';
 
 // Column
 $_['column_date_added']  = 'დამატების თარიღი';
